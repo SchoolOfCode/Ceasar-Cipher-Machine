5ac6ea74683a61b00178e52e85d6d74a00f5f84a
Maria-C1 <mariacrawford789@gmail.com>
2021-12-22 16:52:09 +0000

    wrapped word included in test

--- main.js
+++ main.js
@@ -16,63 +16,79 @@ z- b
 
 // LetterPlusTwoInTheAlphabet = alp[indexOfLetter+2]
 
-
 // const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 //Given a word, return the world encoded in the ceasar cypher(each letter is +2 of its position in the alpabet)
-// dogs >>> fqiu 
+// dogs >>> fqiu
 
 //Handle edge cases y,z and numbers, spaces
 
-
-
 //👉 Write the function your CodeWarriors will start with below here:
 
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+// normal function
+export function getEncodedWord(word, encoding) {
+  if (encoding) {
+    let wordAsArray = word.split(""); //cats >> [c,a,t,s]
+    console.log(wordAsArray); //works
 
-const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    let encodedWord = wordAsArray.map(function (v, i, array) {
+      const letterIndex = alphabet.findIndex((letter) => letter === v);
+      // if letters are y or z, run index up to 25 then restart at 0
+      if (letterIndex === 24) {
+        return alphabet[0];
+      } else if (letterIndex === 25) {
+        return alphabet[1];
+      } else {
+        return alphabet[letterIndex + 2];
+      }
+    });
 
-export function getEncodedWord(word, encoding){
+    console.log(encodedWord);
+    return encodedWord.join("");
+  } else {
+    let wordAsArray = word.split(""); //cats >> [c,a,t,s]
+    console.log(wordAsArray); //works
 
-    if(encoding){
-        let wordAsArray = word.split("") //cats >> [c,a,t,s]
-        console.log(wordAsArray);//works
+    let encodedWord = wordAsArray.map(function (v, i, array) {
+      // go through each letter 'c' find the index of c alp,
+      // const letterIndex =  alphabet.findIndex(v => v === v)
+      // console.log(letterIndex)
+      const letterIndex = alphabet.findIndex((letter) => letter === v);
+      return alphabet[letterIndex - 2];
+    });
 
-        let encodedWord = wordAsArray.map(function(v,i,array){ // go through each letter 'c' find the index of c alp,
-                // const letterIndex =  alphabet.findIndex(v => v === v)
-                // console.log(letterIndex)
-                const letterIndex = alphabet.findIndex(letter => letter === v) 
-                return alphabet[letterIndex+2]
-                })
+    console.log(encodedWord);
+    return encodedWord.join("");
+  }
+}
 
+getEncodedWord("dog", true);
 
-        console.log(encodedWord);
-        return encodedWord.join("")
-
-             }else{ 
-
-
-                let wordAsArray = word.split("") //cats >> [c,a,t,s]
-                console.log(wordAsArray);//works
-        
-                let encodedWord = wordAsArray.map(function(v,i,array){ // go through each letter 'c' find the index of c alp,
-                        // const letterIndex =  alphabet.findIndex(v => v === v)
-                        // console.log(letterIndex)
-                        const letterIndex = alphabet.findIndex(letter => letter === v) 
-                        return alphabet[letterIndex-2]
-                        })
-        
-        
-                console.log(encodedWord);
-                return encodedWord.join("")
-        
-
-             }
-        
-    }
-    
-
-
-
-
-
-getEncodedWord("dog", true)
--- main.test.js
+++ main.test.js
@@ -1,50 +1,44 @@
-import {getEncodedWord} from "./main.js"
-
+import { getEncodedWord } from "./main.js";
 
 //👉 Write your tests below here:
 // const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 //dogs >>> fqiu
 const wordsAndOutputNormal = [
-    ["dog", true, "fqi"],
-    ["racoon", true, "tceqqp"],
-    ["wpkeqtp", false, "unicorn"],
-    
-]
+  ["dog", true, "fqi"],
+  ["racoon", true, "tceqqp"],
+  ["wpkeqtp", false, "unicorn"],
+];
 
-const wordsAndOutputWrap = [ ["zebra", true, "bgdtc"], 
-                           ["zorse", true, "bqtug"],
-                           ["Yak", true, "acm"] ]
+const wordsAndOutputWrap = [
+  ["zebra", true, "bgdtc"],
+  ["zorse", true, "bqtug"],
+  ["Yak", true, "acm"],
+];
 
+const wordsAndOutputCapitals = [
+  ["zebra", true, "bgdtc"],
+  ["Dragon", true, "ftciqp"],
+];
 
-const wordsAndOutputCapitals = [ ["zebra", true, "bgdtc"], 
-                           ["Dragon", true, "ftciqp"] ]
+describe("encoder/decoder", () => {
+  //Testing for wraps
 
+  // it.each(wordsAndOutputWrap)('should wrap around the alpabet when given %a, set to encode by %b, and return %c',
+  // (input, mode, expected) => {
 
+  //         expect(getEncodedWord(input,mode)).toBe(expected)
+  // }
+  // )
 
-describe("encoder/decoder" ,()=>{
+  //Testing for capitals
 
-    //Testing for wraps
+  //Testing normal conditions
 
-    // it.each(wordsAndOutputWrap)('should wrap around the alpabet when given %a, set to encode by %b, and return %c', 
-    // (input, mode, expected) => {
-
-    //         expect(getEncodedWord(input,mode)).toBe(expected)
-    // }
-    // )
-
-    
-
-    //Testing for capitals
-
-    //Testing normal conditions
-
-
-    it.each(wordsAndOutputNormal)('should encode or decode %a when given the mode %b', 
+  it.each(wordsAndOutputNormal)(
+    "should encode or decode %a when given the mode %b",
     (input, mode, expected) => {
-
-            expect(getEncodedWord(input,mode)).toBe(expected)
+      expect(getEncodedWord(input, mode)).toBe(expected);
     }
-    )
-
-})
+  );
+});
